d977c309dba06b4d9146165ec56c2a7797dbf7f8
GaoJianXin520 <1603452601@qq.com>
2022-01-20 17:55:55 +0800

    搭建html框架

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        environment: {
+            arrowFunction: false,
+            const: false
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, 
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                            chrome: '88',
+                                        },
+                                        corejs: '3',
+                                        useBuiltIns: 'usage'
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'
+                ],
+                exclude: /node_modlues/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'postcss-preset-env',
+                                        {
+                                            browsers: 'last 2 versions'
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
